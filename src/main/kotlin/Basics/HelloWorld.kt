fdfc2c24356decb052a587a5415eb1ea282e84f5
stefan <stefanbayne@gmail.com>
2022-08-11 16:46:30 -0400

    Hello World and Welcome.

---
+++ src/main/kotlin/Basics/HelloWorld.kt
@@ -0,0 +1,25 @@
+package Basics
+
+/*
+Each Kotlin file must have a main function just like in Java.
+ */
+fun main(args: Array<String>) {
+
+    /*
+    println() is used to print output to the console.
+     */
+    println("Hello world!")
+    println("Welcome to the Basic Kotlin Demo!")
+    println("--------------------------------")
+    println("Here we will go over everything you need in order to learn Kotlin!")
+}
+
+/*
+
+Output:
+
+Welcome to the Basic Kotlin Demo!
+--------------------------------
+Here we will go over everything you need in order to learn Kotlin!
+
+ */
