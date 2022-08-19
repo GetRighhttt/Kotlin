f35149b875595bed519f334d1863424a488918a0
stefan <stefanbayne@gmail.com>
2022-08-19 14:20:35 -0400

    Loops

---
+++ src/main/kotlin/basics/IfStatementsAndLoops.kt
@@ -0,0 +1,55 @@
+package basics
+
+fun main(args: Array<String>) {
+
+    /**
+     * This is going to be a basic example of if
+     * statements in Kotlin.
+     *
+     * for loops are for iterating through collections
+     *
+     * while loops execute while a condition is met
+     *
+     * do-while loops do some logic while the condition is met. They
+     * always run at least one time.
+     */
+    val y = 10
+    val x = 10
+
+    if(x == y) {
+        println("This is true.")
+    }
+    else if(x == 11) {
+        println("This is not true.")
+    }
+
+    val newAnswer = if (y == 10) println(y) else println(y + 1)
+
+    // for loop
+    var count = 0
+    for (i in 0..10) {
+        println(count++)
+    }
+
+    // while loop
+    while (count < x) {
+        count++
+        println("This is a while loop.")
+    }
+
+    // do-while
+    do {
+        println("This is a do while loop.")
+    } while (y > x)
+
+    /*
+    When statements are like switch statements except they
+    come with more functionality than switch statements.
+     */
+    when(x) {
+        in 1..10 -> println("In between 1 and 10")
+        in 10..20 -> println("In between 10 and 20")
+        else -> println("Above 20.")
+    }
+
+}
