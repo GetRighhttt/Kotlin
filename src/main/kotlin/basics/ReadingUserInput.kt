4addff9ca43dfa59e33fae932c382f380154134a
stefan <stefanbayne@gmail.com>
2022-08-19 14:43:14 -0400

    Reading user input.

---
+++ src/main/kotlin/basics/ReadingUserInput.kt
@@ -0,0 +1,34 @@
+package basics
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    /*
+    We can read user input in the string in a few ways:
+
+    readLine() always returns a string, and we must make sure
+    it's not null with a ?.
+
+    Scanner class in java reads in any input
+     */
+    println("Enter some text you want reversed:")
+    val userInput = readLine()
+    println(userInput?.reversed())
+
+    val scanner = Scanner(System.`in`)
+    println("Enter a number: ")
+    val num = scanner.nextInt()
+    println("This is the number minus 10: ${num - 10}")
+
+    /**
+     *
+     * Output:
+     *
+     * Enter some text you want reversed:
+    Multiversus is a great game
+    emag taerg a si susrevitluM
+    Enter a number:
+    304
+    This is the number minus 10: 294
+     */
+}
