6e120dc0167409d5a118f2177865fdce133a45bc
Stefan Bayne <stefanbayne@gmail.com>
2023-03-15 13:06:38 -0400

    Working with more oop examples.

--- src/main/kotlin/oop/StudentExample.kt
+++ src/main/kotlin/oop/StudentExample.kt
@@ -1,5 +1,8 @@
 package oop
 
+import basics.getName
+import java.util.stream.Collectors
+
 /*
 In Kotlin, when we pass in values and variables in the primary constructor, getters and setters
 are automatically generated in the compiler for us to use in main.
@@ -7,7 +10,7 @@ are automatically generated in the compiler for us to use in main.
 To allow a class to be inherited from, in Kotlin we have to use the open keyword. And that goes
 for properties of classes as well.
  */
-open class Student(open val name: String, var age: Int)
+open class Student(open val name: String, open var age: Int)
 
 /*
 If we want to pass in a mutable value not in the primary constructor, we declare it in the class
@@ -15,6 +18,17 @@ body, and we must provide an initial value.
  */
 class Teacher(val name: String) {
     var age: Int = 0
+    var studentName: String = "Zion"
+    var studentAge: Int = 5
+
+    /*
+    Below we instantiate a student in the teacher's class, because a teacher has students.
+    this is typically how composition works.
+     */
+    private val newStudent = Student(studentName, studentAge) // an example of composition "has-a" relationship.
+
+    fun printStudentName(): String = newStudent.name
+    fun printStudentAge(): Int = newStudent.age
 }
 
 /*
@@ -24,10 +38,12 @@ we can define an explicit setter for this.
 Here we inherit from the Student class because a new player is still a student. Since the name parameter
 has the same name as the student, we have to override it so that it can be used. We could also just
 change the parameter to a different name but for demo purposes, we won't.
- */
-class MyNewPlayer(override val name: String) : Student(name, 30) {
 
-    var score: Int = 0
+We also implement the BallRoller interface and call the rollBall() method in the main function.
+ */
+class MyNewPlayer(override val name: String, override var age: Int) : Student(name, age), BallRoller {
+
+    var score: Int = 20
         set(value) {
             field = if (value >= 0) {
                 value
@@ -48,16 +64,47 @@ Ex: class BasketballPlayer(val name: String) : BallRoller {
 }
  */
 interface BallRoller {
-    fun rollBall(): Int
+    fun rollBall(times: Int): Int = times
 }
 
 fun main() {
 
-    val myStudent = Student("John", 25)
-    val yourStudent = Student("Stefan", 28)
-    val thisNewPlayer = MyNewPlayer("Bobby")
-    thisNewPlayer.score = 20
-
+    val myStudent = Student("DJ", 25)
+    val yourStudent = Student("Stefan", 26)
+    val thisNewPlayer = MyNewPlayer("Luther", 10)
     val thisTeacher = Teacher("Ayesha")
-    thisTeacher.age = 80
+
+    /*
+    Below we are creating a list of all the students, and because thisNewPlayer inherits from Student,
+    it still doesn't cause an error in the list although its of type MyNewPlayer.
+     */
+    val theseStudents = mutableListOf<Student>(myStudent, yourStudent, thisNewPlayer)
+    val filterByAge = theseStudents.stream()
+        .filter { it.age == 25 }
+        .collect(Collectors.toList())
+        .sortedBy { it.age }
+
+    /*
+    Below we show different ways to print multiple lines of strings again.
+     */
+    println(
+        """
+        |${yourStudent.name} and ${myStudent.name} have been friends for over 10 years.
+        |${yourStudent.name} is ${yourStudent.age} while ${myStudent.name} is ${myStudent.age}!
+        |""".trimMargin()
+    )
+    println(
+        "${thisNewPlayer.name} is ${thisNewPlayer.age} years old! ${thisNewPlayer.name}" +
+                " rolled the ball ${thisNewPlayer.rollBall(10)} times for a score of" +
+                " ${thisNewPlayer.score} points!"
+    )
+    /*
+    Below we display how composition can be used to display the student declared in the
+    teacher's class.
+     */
+    println(
+        "${thisTeacher.printStudentName()} is in ${thisTeacher.name}'s " +
+                "Math class, and is ${thisTeacher.printStudentAge()} years old!"
+    )
+
 }
