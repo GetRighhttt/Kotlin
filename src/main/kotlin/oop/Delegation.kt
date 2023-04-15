6a819751ccb2e94774b70bf7f07da636388269d3
Stefan Bayne <stefanbayne@gmail.com>
2023-04-15 12:22:07 -0400

    delegation cleanup.

--- src/main/kotlin/oop/Delegation.kt
+++ src/main/kotlin/oop/Delegation.kt
@@ -16,34 +16,49 @@ interface DelegationExample {
 
 open class DelegateBase(var a: String) : DelegationExample {
     init {
-        println("Delegate base class has been extended..") // called first
+        println("Delegate Base class has been extended...") // called first
     }
 
-    override val message: String = "Here is the abstract delegate message" // called third
+    override val message: String
+        get() = """
+            Here is the message from the base class before delegation, and
+            this is going to be called from the returnDelegateBase method.
+        """.trimIndent() // called last
+
     override fun returnDelegateBase() {
-        println("Delegate Base")
+        println(message)
     }
 }
 
 class DerivedDelegate(base: DelegationExample) : DelegationExample by base {
     init {
-        println("Beginning Delegation..") // called second
+        println("Beginning Delegation from Derived Delegate class...") // called second
     }
-    override fun returnDelegateBase() {
-        println("Delegation has been achieved...") // called last, and shows delegation
-    }
+
+    /*
+    When we call the message in main, this will display as a delegation of the message that we created
+    in the class above. We delegated the responsibility of providing a message to this class.
+     */
+    override val message: String
+        get() = "Here is the message from the Derived Delegate class proving that Delegation has been achieved."
 }
 
 fun main() {
     val delegationObject = DelegateBase("Starting...")
     val delegationResult = DerivedDelegate(delegationObject)
-    println(delegationResult.message)
-    println(delegationResult.returnDelegateBase()) // here is where we show the delegation for the return method.
+    println(delegationResult.message) // delegation
+    println(delegationResult.returnDelegateBase()) // base class
 }
 
 /*
-Delegate base class has been extended..
-Beginning Delegation..
-Here is the abstract delegate message
-Delegation has been achieved...
+In the output. we see that each init block is called first in order.
+Then, when we call the message, we see that the Derived Delegate message is the one being printed because
+we DELEGATED the message to that class.
+
+
+Delegate Base class has been extended...
+Beginning Delegation from Derived Delegate class...
+Here is the message from the Derived Delegate class proving that Delegation has been achieved.
+Here is the message from the base class before delegation, and
+this is going to be called from the returnDelegateBase method.
  */
