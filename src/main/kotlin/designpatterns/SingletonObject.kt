0bfd57e9c82e2b41fdb77ca44a9db264f5576e1b
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-01 08:55:33 -0400

    singleton design pattern created.

---
+++ src/main/kotlin/designpatterns/SingletonObject.kt
@@ -0,0 +1,25 @@
+package designpatterns
+
+/*
+A "Singleton" is about limiting memory usage and resource allocation. When we create objects that are the same,
+we are being wasteful and using unnecessary resources. This is a problem when we want our apps to scale and need
+additional resources in order to do so.
+
+In Kotlin, this is mad easy for us by using the 'object' keyword. All of our requirements are met in Kotlin by using
+the object keyword.
+Singleton requirements:
+
+1. Should have exactly one instance.
+2. Instance should be accessible by all parts of the system.
+3. Thread safe
+4. Performant
+5. Lazy initialization
+
+Object keyword is basically like a class except it cant have a constructor and it cannot be instantiated.
+ */
+object SingletonObject {
+
+    init {
+        // We can use an init block if we need to something to happen when the object is called.
+    }
+}
