5912df758aeacfb19d677f15599726a58118fe9b
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-01 12:26:17 -0400

    prototype example.

--- src/main/kotlin/designpatterns/creational/Prototype.kt
+++ src/main/kotlin/designpatterns/creational/Prototype.kt
@@ -1,2 +1,20 @@
 package designpatterns.creational
 
+/*
+The prototype method hinges on the idea of copying an object, and extending its functionality so other
+objects can be manipulated by that clone. It's best seen when creating a data class, and using the copy
+method.
+ */
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+    val newPerson = Person("Stefan", 22)
+    val oldPerson = newPerson.copy(name = "bob", age = 50)
+
+    val (names, ages) = oldPerson
+    println(names)
+    println(ages)
+}
