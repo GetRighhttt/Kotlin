411364babfd6aec88389746ad8919c625274ac69
Stefan Bayne <stefanbayne@gmail.com>
2023-05-01 22:56:16 -0400

    factory commenting and refactoring.

--- src/main/kotlin/designpatterns/creational/TheFactoryMethod.kt
+++ src/main/kotlin/designpatterns/creational/TheFactoryMethod.kt
@@ -7,13 +7,18 @@ Kotlin language in most areas, however it is important to know how the factory p
 need to create this for our own use case in the future.
 
 The factory pattern hides the complexity of creating and selecting the right object for the job.
-Essentially, it works to create objects and hides the complexity of creating objects of the same family.
+
+When you think of the factory pattern, think of a factory: creating objects based on given conditions. When an
+object meets a condition, it is created, otherwise it is not created. The determining factor is abstracted away
+from the client. We often see this demonstrated with sealed classes and handling state using when statements.
  */
 
 interface FileParser
 
-class XmlFileParser : FileParser
-class JsonFileParser : FileParser
+sealed class FileParserState {
+    class XmlFileParser : FileParser
+    class JsonFileParser : FileParser
+}
 
 /* The concept of the factory -> creates a Product */
 interface FileParserFactory {
@@ -27,8 +32,8 @@ class CompanionObjectFileParserFactory : FileParserFactory {
     companion object : FileParserFactory{
         override fun createFromFileName(fileName: String) =
             when (fileName.substringAfterLast('.')) {
-                "xml" -> XmlFileParser()
-                "json" -> JsonFileParser()
+                "xml" -> FileParserState.XmlFileParser()
+                "json" -> FileParserState.JsonFileParser()
                 else -> throw Exception("I don't know how to deal with $fileName.")
             }
     }
