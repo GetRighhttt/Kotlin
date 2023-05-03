05f020be453279d4ed1c78772e56719b85085c55
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-03 18:05:54 -0400

    refactored tuples and added some more logic.

--- src/main/kotlin/advanced/functional/Tuples.kt
+++ src/main/kotlin/advanced/functional/Tuples.kt
@@ -15,30 +15,38 @@ fun main() {
 
     // destructing a pair using destructive declaration
     val (key, value) = pair
-    println("$key, $value") // a, 1
+    println("Printing a key-value pair: $key, $value") // a, 1
 
     val anotherPair = Pair("one" to 1, "two" to 2)
     val (one, two) = anotherPair
-    println("$one and $two") // (one, 1) and (two, 2)
+    println("This object prints like : $one and $two") // (one, 1) and (two, 2) - objects
+
+    println("The keys are: ${one.first} and ${two.first} ") // "one and two" - keys
+    println("The values are: ${one.second} and ${two.second} ") // "1 and 2" - values
 
     /*
     There is also a triple tuple that we can work with in Kotlin and other languages that
     encompasses the concept of immutability.
      */
-    val thisTriplePair = Triple("a", "b ", "c")
-    println(thisTriplePair.toList()) // [a, b , c]
+    val thisTriplePair = Triple("a" to 11, "b " to 12, "c" to 13)
+    val (a, b, c) = thisTriplePair
+    println("The key-value pairs are: (${a.first}, ${a.second}) | (${b.first}, ${b.second}) | (${c.first}, ${c.second})")
 
 
     /*
     Another destructuring example with data classes which act as tuples as well if using values.
     Below we create a data class acting as a Triple.
      */
-    data class TupleExample(val first: Int, val second: Int, val third: Int)
+    data class TupleExample(val first: String, val second: String, val third: String)
 
-    val tupleExample = TupleExample(1, 2, 3)
-
+    val tupleExample = TupleExample("first", "second", "third")
     val (first, second, third) = tupleExample
-    println("$first, $second, $third") // 1, 2, 3
+    println("$first, $second, $third")
 
-
+//    Printing a key-value pair: a, 1
+//    This object prints like : (one, 1) and (two, 2)
+//    The keys are: one and two
+//    The values are: 1 and 2
+//    The key-value pairs are: (a, 11) | (b , 12) | (c, 13)
+//    first, second, third
 }
