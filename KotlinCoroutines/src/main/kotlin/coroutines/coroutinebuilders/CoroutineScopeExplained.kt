dd389d4e0bd30676e4866183bbc64cdcddcc8c71
Stefan Bayne <stefanbusiness95@gmail.com>
2023-05-16 16:03:51 -0400

    coroutine scope.

--- KotlinCoroutines/src/main/kotlin/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
+++ KotlinCoroutines/src/main/kotlin/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
@@ -2,4 +2,5 @@ package coroutines.coroutinebuilders
 
 /*
 When we want to use a coroutine inside a suspending function, we use CoroutineScope.
+Coroutine Scopes allow us also to cancel multiple coroutines at the same time.
  */
