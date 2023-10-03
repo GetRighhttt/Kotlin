a799e56d95fe84622a62824e2bf1c825078c3dce
Stefan Bayne <stefanbusiness95@gmail.com>
2023-10-03 15:52:59 -0400

    increasing readability.

--- src/main/kotlin/advancedkotlin/functional/morefunctions/InfixFunctionsEx.kt
+++ src/main/kotlin/advancedkotlin/functional/morefunctions/InfixFunctionsEx.kt
@@ -8,8 +8,8 @@ Infix functions must be defined inside a class or an extension function to an ex
 
 // declaring extension functions
 infix fun String.newName(name: String): String = name
-infix fun Int.theirAgeTimesThree(age: () -> Int): Int = age() // function type that returns an Int
-infix fun String.getMiddleName(name: String): String = newName(name)
+infix fun Int.multipliedByThree(age: () -> Int): Int = age() // function type that returns an Int
+infix fun String.setMiddleName(name: String): String = newName(name)
 
 fun main() {
     // created a variable with no initial name
@@ -18,7 +18,7 @@ fun main() {
     // example of using infix function to assign name and age times three
     val actualName = tempName newName "Stefan"
 
-    val theirAge = 12 theirAgeTimesThree {
+    val theirAge = 12 multipliedByThree {
         36
     }
 
@@ -26,14 +26,14 @@ fun main() {
 
     // using let with also scope functions to assign middle name
     actualName.let {
-        it getMiddleName "John"
+        it setMiddleName "John"
     }.also {
         println("My middle name is $it.")
     }
 
     // using let scope function to multiply it once again by 3
     theirAge.let {
-        it theirAgeTimesThree {
+        it multipliedByThree {
             it * 3
         }
     }.also {
