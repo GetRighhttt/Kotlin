2b33e7feec0c7cb0d785cfee2466708ef2bc930f
Stefan Bayne <stefanbusiness95@gmail.com>
2023-10-18 11:36:56 -0400

    working with generics in a better way.

--- src/main/kotlin/oop/generics/GenericsExamples.kt
+++ src/main/kotlin/oop/generics/GenericsExamples.kt
@@ -4,13 +4,47 @@ package oop.generics
 Let's go over a few Generic examples
  */
 
-open class Animal <T> (val t: T)
+// Invariant type which is just a basic generic
+interface BasicGeneric<T> {
+     fun genericType()
+}
 
-class Dog
-class Fish
-open class Bird
-class Eagle: Bird()
+class Basic: BasicGeneric<Any> {
+     override fun genericType() {
+          println("Just a generic class.")
+     }
+
+}
+
+// Consumer interface that only consumes values I.E no return type
+interface Consumer<in T> {
+     fun consumer(item: T)
+}
+
+class ShowConsumer: Consumer<String> {
+     override fun consumer(item: String) {
+          println("$item has been Consumed as input.")
+     }
+
+}
+
+// Producer interface that only produces values I.E has a return type
+interface Producer<out T> {
+     fun producer(): T
+}
+
+class ShowProducer: Producer<String> {
+     override fun producer(): String = "We are producing this string without any input."
+}
 
 fun main() {
-     val myAnimal = Animal(Dog()) // declaring an invariant animal object of type dog
+
+     val genericExample = Basic()
+     genericExample.genericType() // Just a generic class.
+
+     val stringConsumer = ShowConsumer()
+     stringConsumer.consumer("Item") // Item has been Consumed as input.
+
+     val stringProducer = ShowProducer()
+     println(stringProducer.producer()) // We are producing this string without any input.
 }
