8def0a0a91c74da0cdfea63ca80e4f3a4c8f3626
Stefan Bayne <stefanbusiness95@gmail.com>
2023-12-15 05:47:42 -0500

    new type alias example.

--- src/main/kotlin/oop/TypeAliasesEx.kt
+++ src/main/kotlin/oop/TypeAliasesEx.kt
@@ -4,11 +4,22 @@ package oop
 // TypeAliases can make the code more concise and more readable. Could save on time as well.
 
 data class SuperLargeNameThatIWontWriteAgain( val disaster: String)
+data class ExampleTypeAlias(val example: String)
 
 typealias longName = SuperLargeNameThatIWontWriteAgain
+typealias newExample = ExampleTypeAlias
 fun main() {
 
     // allows us to create an object with the abbreviation
     val theName = longName("terrible disaster")
     println(theName)
+
+    val thisExample = newExample("this is an example of another typealias").also(::println)
+    println(thisExample.toString().uppercase())
+
+    /*
+    SuperLargeNameThatIWontWriteAgain(disaster=terrible disaster)
+    ExampleTypeAlias(example=this is an example of another typealias)
+    EXAMPLETYPEALIAS(EXAMPLE=THIS IS AN EXAMPLE OF ANOTHER TYPEALIAS)
+     */
 }
