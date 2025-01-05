db3b3303c65cdaed1de01a92c23ad5951f5404b2
Stefan Bayne <stefanbusiness95@gmail.com>
2025-01-04 22:03:42 -0500

    chore; adding chapter 1 comments

---
+++ src/main/kotlin/essentialKotlin/chapter1.kt
@@ -0,0 +1,25 @@
+package essentialKotlin
+
+/**
+ * @author Stefan Bayne
+ * This file will represent some of Kotlin's most essential tips all from the book "Essential Kotlin"
+ * by Marcin Moskala.
+ * Date: January 4, 2025
+ */
+
+/**
+ * ------------------------------------------------------------------------------------------------
+ * Chapter 1 - Safety
+ * ------------------------------------------------------------------------------------------------
+ */
+
+/**
+ * Item #1 - Limit Mutability
+ *
+ * Mutable states have  core problems:
+ * a. Harder to understand and debug a program with many mutating points;
+ * b. Mutability makes it harder to reason about code;
+ * c. Mutable states require proper synchronization in multithreaded programs;
+ * d. Mutable elements are harder to tests;
+ * e. Other objects need to be notified when a state mutates.
+ */
