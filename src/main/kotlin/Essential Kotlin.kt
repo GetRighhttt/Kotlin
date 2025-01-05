4ff546a04c6565bba48bd219ab740f7d7f65db0b
Stefan Bayne <stefanbusiness95@gmail.com>
2025-01-04 20:56:26 -0500

    feat: inital commit for essential kotlin

---
+++ src/main/kotlin/Essential Kotlin.kt
@@ -0,0 +1,14 @@
+/**
+ * This file will represent some of Kotlin's most essential tips all from the book "Essential Kotlin"
+ * by Marcin Moskala.
+ */
+
+/**
+ * ------------------------------------------------------------------------------------------------
+ * Chapter 1 - Safety
+ * ------------------------------------------------------------------------------------------------
+ */
+
+/**
+ *  Item #1 - Limit Mutability
+ */
